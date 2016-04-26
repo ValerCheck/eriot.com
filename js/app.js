a549c3b5adfbb039ab842cc9069c22ac0df958c3
valercheck <mypowerismind@gmail.com>
2016-04-27 00:39:23 +0300

    Add style and js to Title block

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var eriotApp = angular.module('eriotApp',[]);
+
+eriotApp.controller('LandingController',function($scope){
+
+	$scope.menu = [{
+		'title'	: 'Home',
+		'link'	: '#'
+	},{
+		'title'	: 'Services',
+		'link'	: '#', 
+	},{
+		'title'	: 'Expertise',
+		'link'	: '#'
+	},{
+		'title'	: 'Team',
+		'link'	: '#'
+	},{
+		'title'	: 'Awards/Cases',
+		'link'	: '#'
+	},{
+		'title'	: 'Contacts',
+		'link'	: '#'
+	}];
+
+});
