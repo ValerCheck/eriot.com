a708e2afafb10725ca9432ae52d1978f4b7115c9
valercheck <mypowerismind@gmail.com>
2016-05-31 11:39:24 +0300

    [Devices|Expertise] Add images with description to Devices, Add f*cking animation to Expertise

--- js/app.js
+++ js/app.js
@@ -10,6 +10,89 @@ $(document).ready(function(){
 				width: '100%'
 			}, 3000);
 		}
+
+		if ($(document).scrollTop()+$(window).height() - 100 >= $('.exp-line-area').offset().top) {
+			var elements = [];
+			var notColor = [];
+			var lengths  = [];
+			var speeds 	 = [];
+			var condElem = $('.exp-line-area > div.line:last-child .color');
+			if (parseInt(condElem.css('width')) == 0) {
+
+				lengths.push(parseFloat($('.exp-line-area > div.line:first').css('width')));
+				var len = lengths[0];
+
+				elements.push('.exp-line-area > div.line:first .color');
+				
+				for (var i = 1; i < 3; i++) {
+					
+					var first = '#'+ (2*i-1);
+					var middle = '.expertise-item:nth-child('+(i+1)+')';
+					var last = '#'+ (2*i);
+
+					lengths.push(parseFloat($(first).parent().css('width')));
+					lengths.push(parseFloat($(middle).css('width')));
+					lengths.push(parseFloat($(last).parent().css('width')));
+
+					len += lengths[1];
+					len += lengths[2];
+					len += lengths[3];
+
+					elements.push(first 	+ '.color');
+					elements.push(middle + ' .front-color');
+					elements.push(last   + '.color');
+				}
+
+				var lineFirst = '.exp-line-area .line.for-mobile:first';
+				var lineLast =	'.exp-line-area .line.for-mobile:last'; 
+				lengths.push(parseFloat($(lineFirst).css('width')));
+				lengths.push(parseFloat($(lineLast).css('width')));
+
+				elements.push(lineFirst);
+				elements.push(lineLast);
+
+				for (var i = 1; i < 3; i++) {
+
+					var first = '#'+ (3+2*i);
+					var middle = '.expertise-item:nth-child('+(i+5)+')';
+					var last = '#'+ (4+2*i);
+
+					lengths.push(parseFloat($(first).parent().css('width')));
+					lengths.push(parseFloat($(middle).css('width')));
+					lengths.push(parseFloat($(last).parent().css('width')));
+
+					len += lengths[6];
+					len += lengths[7];
+					len += lengths[8];
+
+					elements.push(first 	+ '.color');
+					elements.push(middle + ' .front-color');
+					elements.push(last   + '.color');
+				}
+
+				lengths.push(parseFloat($('.exp-line-area > div.line:last').css('width')));
+
+				len += lengths[9];
+				var velocity = len/3000;
+				speeds = lengths.map(function(el){
+					return el/velocity;
+				});
+
+				elements.push('.exp-line-area > div.line:last .color');
+
+				var arr = elements;
+				
+				function fill(id){
+				    if(id<arr.length){
+				    	var elem = $(arr[id]);
+				    	if (elem.css('display') == 'none') fill(id+1);
+				    	else elem.animate({width:'100%'}, speeds[id],"linear", function(){ fill(id+1)});
+				    } 
+				}
+				fill(0);
+				
+			}
+		}
 	});
 
 	$('.step-slider').on('beforeChange',function(event,slick,current,next){
